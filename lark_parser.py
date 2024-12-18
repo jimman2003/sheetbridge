67738129926dc8711db62e3b1f1d0059080ef974
jimman2003 <58356745+jimman2003@users.noreply.github.com>
2024-12-19 00:37:55 +0200

    make the compiler work

--- lark_parser.py
+++ lark_parser.py
@@ -1,7 +1,7 @@
 from zipfile import ZipFile
 import xml.etree.ElementTree as ET
 
-from lark import Lark,Transformer
+from lark import Lark,Transformer,Token
 
 
 
@@ -13,10 +13,23 @@ class PythonTransformer(Transformer):
         [start,end] =r 
         return f'xl("{start}:{end}")'
       def cell(self,r):
-        (_,cell),=r
+        (_,cell)=r
         return f'xl("{cell}")'
-      def sum(self,s):
-        return f'{s[0]}.sum()'
+      
+      def function(self,s):
+        [excel_function,args]=s
+        changed_names={
+            'AVERAGE':'mean'
+        }
+        python_name=changed_names.get(excel_function,excel_function.lower())
+        match excel_function.type:
+          case 'SUFFIX_FUNCTION':
+            return f'{args}.{python_name}()'
+          case 'PREFIX_FUNCTION':
+            return f'{python_name}({args})'
+         
+      
+    
 
 with ZipFile('../test.xlsx') as myzip:
     with myzip.open('xl/worksheets/sheet1.xml') as myfile:
@@ -26,10 +39,11 @@ root=ET.fromstring(content)
 def get_formulas():
     ns={'ns':'http://schemas.openxmlformats.org/spreadsheetml/2006/main'}
     for tag in root.iterfind(".//ns:c/ns:f",ns):
-                 yield tag.text
+      if "_xlfn._xlws.PY" not in tag.text:
+          yield tag.text
 
 for formula in get_formulas():
-    #print(formula)
+    print(formula)
     tree =l.parse(formula)
-    print(tree.pretty())
+    print(tree)
     print(PythonTransformer().transform(tree))
