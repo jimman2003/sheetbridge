bdf19cf3785c77de73d2351d03aab8051c3e3bdf
jimman2003 <58356745+jimman2003@users.noreply.github.com>
2025-03-16 02:07:35 +0200

    add calc token + tests

--- lark_parser.py
+++ lark_parser.py
@@ -1,7 +1,7 @@
 from zipfile import ZipFile
 import xml.etree.ElementTree as ET
 
-from lark import Lark,Transformer
+from lark import Lark, Transformer
 import shutil
 import openpyxl
 import openpyxl.cell
@@ -12,45 +12,48 @@ import openpyxl.worksheet.cell_range
 import openpyxl.worksheet.formula
 
 
-l = Lark.open('grammar.lark')
+l = Lark.open("grammar.lark", start="formula")
+
 
 class PythonTransformer(Transformer):
-      def range(self,r):
-        [start,end] =r 
-        return f'xl("{start}:{end}")'
-      def cell(self,r):
-        (_,cell)=r
-        return f'xl("{cell}")'
-      
-      def function(self,s):
-        [excel_function,args]=s
-        changed_names={
-            'AVERAGE':'mean'
-        }
-        python_name=changed_names.get(excel_function,excel_function.lower())
-        match excel_function.type:
-          case 'SUFFIX_FUNCTION':
-            return f'{args}.{python_name}()'
-          case 'PREFIX_FUNCTION':
-            return f'{python_name}({args})'
-         
-def transform_formula(formula):
-  tree = l.parse(formula)
-  python_formula = PythonTransformer().transform(tree)
-  return python_formula      
-    
+    def RANGE(self, r):
+        return f'xl("{r}")'
 
-original_file = '../test.xlsx'
+    def CELL_REF(self, r):
+        return f'xl("{r}")'
+
+    def function(self, s):
+        [excel_function, args] = s
+        changed_names = {"AVERAGE": "mean"}
+        python_name = changed_names.get(excel_function, excel_function.lower())
+        match excel_function.type:
+            case "SUFFIX_FUNCTION":
+                return f"{args}.{python_name}()"
+            case "PREFIX_FUNCTION":
+                return f"{python_name}({args})"
+    
+    def calc(self, s):
+        [left, operator, right] = s
+        return f"{left}{operator}{right}"
+
+def transform_formula(formula):
+    tree = l.parse(formula)
+    python_formula = PythonTransformer().transform(tree)
+    return python_formula
+
+
+original_file = "../test.xlsx"
 original_excel = openpyxl.load_workbook(original_file)
 sheet = original_excel.active
 for value in sheet.iter_rows():
-  for cell in value:
-    if cell.data_type == 'f':
-      formula = cell.value
-      if not isinstance(formula, openpyxl.worksheet.formula.ArrayFormula):
-        python_formula = transform_formula(formula)
-        #cell.value = "_xlfn._xlws.PY(1,1,C4,C4)"
-        sheet[cell.coordinate] =  openpyxl.worksheet.formula.ArrayFormula(cell.coordinate,python_formula)
-        print(python_formula)
+    for cell in value:
+        if cell.data_type == "f":
+            formula = cell.value
+            if not isinstance(formula, openpyxl.worksheet.formula.ArrayFormula):
+                python_formula = transform_formula(formula)
+                # cell.value = "_xlfn._xlws.PY(1,1,C4,C4)"
+                sheet[cell.coordinate] = openpyxl.worksheet.formula.ArrayFormula(
+                    cell.coordinate, python_formula
+                )
+                print(python_formula)
 # original_excel.save('python.xlsx')
-
--- test_transformer.py
+++ test_transformer.py
@@ -10,11 +10,18 @@ l = Lark.open("grammar.lark")
     "formula,python_formula",
     [
         ("=SUM(A1:A2)", 'xl("A1:A2").sum()'),
-        ("=SUM(B1:B2)", 'xl("B1:B2").sum()'),
-        ("=SUM(A3:B3)", 'xl("A3:B3").sum()'),
-        ("=AVERAGE(A1:A2)", 'xl("A1:A2").mean()'),
         ("=AVERAGE(B1:B2)", 'xl("B1:B2").mean()'),
     ],
 )
-def test_suffix(formula: str, python_formula: str):
+def test_suffix_functions(formula: str, python_formula: str):
+    assert transform_formula(formula) == python_formula
+
+@pytest.mark.parametrize(
+    "formula,python_formula",
+    [
+        ("=A1*A2", 'xl("A1")*xl("A2")'),
+        ("=A1+A2+A3", 'xl("A1")+xl("A2")+xl("A3")'),
+    ]
+)
+def test_infix_operators(formula: str, python_formula: str):
     assert transform_formula(formula) == python_formula
