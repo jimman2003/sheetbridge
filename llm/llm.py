61e515cfd5f4addddb44b0ad45c00b536bde0c0e
jimman2003 <58356745+jimman2003@users.noreply.github.com>
2025-09-14 14:20:11 +0300

    made gemini work

---
+++ llm/llm.py
@@ -0,0 +1,64 @@
+from pydantic import BaseModel, Field
+from google import genai
+from google.genai import types
+import dotenv
+import ollama
+from openai import OpenAI
+
+dotenv.load_dotenv()
+system_prompt_formula = """
+You are an expert in creating Python code from spreadsheet formulas. 
+Given the Excel formula, generate the appropriate Python code and provide a brief explanation of how it works.
+"""
+
+
+
+class FormulaResponse(BaseModel):
+    formula: str = Field(..., description="The generated formula as a string.")
+    explanation: str = Field(..., description="A brief explanation of the formula.")
+
+
+gemini_client = genai.Client()
+openrouter_client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OPEN_ROUTER_API_KEY)
+
+def generate_formula_gemini(system_prompt: str, user_prompt: str):
+    response = gemini_client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=user_prompt,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            response_mime_type="application/json",
+            response_schema=FormulaResponse,
+        ),
+    )
+    return response
+
+
+def generate_formula_ollama(system_prompt: str, user_prompt: str):
+    response = ollama.chat(
+        model="qwen2.5-7b-instruct",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        format=FormulaResponse.model_json_schema(),
+    )
+    # Assuming the response content is a JSON string that can be parsed into FormulaResponse
+    return FormulaResponse.model_validate_json(response.message.content)
+
+
+def generate_formula_openrouter(system_prompt: str, user_prompt: str):
+    response = openrouter_client.models.generate_content(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        config={
+            "response_mime_type": "application/json",
+            "response_schema": FormulaResponse,
+        },
+    )
+    return response
--- main.py
+++ main.py
@@ -1,21 +1,63 @@
+from zipfile import ZipFile
 import streamlit as st
 import traditional.lark_parser
 import traditional.utils
+import llm.llm as llm
+
 
 def trad_ui(excel_file):
     prelude = f"import pandas as pd\n\ndf = pd.read_excel('{excel_file.name}')\n"
-    script = traditional.utils.transform_excel(excel_file, traditional.lark_parser.PandasTransformer)
+    script = traditional.utils.transform_excel(
+        excel_file, traditional.lark_parser.PandasTransformer
+    )
     full_script = prelude + "\n".join(script)
     st.code(full_script, language="python")
-    st.download_button("Download Script", data=full_script, file_name="traditional.py", mime="text/x-python")
+    st.download_button(
+        "Download equivalent Python script",
+        data=full_script,
+        file_name="traditional.py",
+        mime="text/x-python",
+    )
+
+
 def llm_ui(excel_file):
-    # new_script = traditional.utils.transform_excel(excel_file)
-    st.code(new_script, language="python")
-    st.download_button("Download Script", data=new_script, file_name="llm.py", mime="text/x-python")
+    selected_provider = st.selectbox(
+        "Select a provider", options=["Local", "Gemini", "OpenRouter"], index=None
+    )
+    with ZipFile(excel_file) as zf:
+        with zf.open(zf.namelist()[0]) as file:
+            excel_content = file.read().decode("utf-8")
+    new_script = None
+    with st.spinner("Generating Python code using LLM..."):
+        if selected_provider == "Local":
+            new_script = llm.generate_formula_ollama(
+                llm.system_prompt_formula,
+                excel_content,
+            )
+        elif selected_provider == "Gemini":
+            new_script = llm.generate_formula_gemini(
+                llm.system_prompt_formula,
+                excel_content,
+            )
+        elif selected_provider == "OpenRouter":
+            new_script = llm.generate_formula_openrouter(
+                llm.system_prompt_formula, excel_content
+            )
+    if new_script:
+        st.code(new_script, language="python")
+        st.download_button(
+            "Download equivalent Python script (LLM)",
+            data=new_script,
+            file_name="llm.py",
+            mime="text/x-python",
+        )
+
 
 excel_file = st.file_uploader("Upload your Excel file", type=["xlsx", "xls"])
 if excel_file:
-    selected_compiler = st.selectbox("Select a compiler", options=["Traditional", "LLM-powered", "Both"])
+    selected_compiler = st.selectbox(
+        "Select a compiler", options=["Traditional", "LLM-powered", "Both"]
+    )
     if selected_compiler == "Traditional":
         trad_ui(excel_file)
     elif selected_compiler == "LLM-powered":
